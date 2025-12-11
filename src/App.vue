58c4d6575c0bb73b6f4a7e67ddb5106579d96a63
mehrnaz98 <mehrnaz.eft98@gmail.com>
2025-12-11 14:53:58 +0330

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -139,23 +139,7 @@
     </section>
 
     <!-- FOOTER -->
-    <footer class="bg-[#59B2AC] py-8 mt-10 shadow-inner">
-      <div class="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between px-6">
-        <ul class="flex gap-6 text-white mb-4 md:mb-0">
-          <li>Home</li>
-          <li>About</li>
-          <li>How It Works</li>
-          <li>Contact</li>
-        </ul>
-        <p class="text-white">
-          © 2025 DotConnectMe – Prototype<br />
-          Built as an open-source project by
-          <a class="text-blue-600 font-semibold" href="https://github.com/mehrnaz98"
-            >Mehrnaz Eftekhari</a
-          >
-        </p>
-      </div>
-    </footer>
+    <Footer />
   </div>
 </template>
 
@@ -165,6 +149,7 @@ import Multiselect from 'vue-multiselect'
 import 'vue-multiselect/dist/vue-multiselect.min.css'
 import jobs from './data/jobs.json'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 // Inputs
 const selectedSkills = ref([])
