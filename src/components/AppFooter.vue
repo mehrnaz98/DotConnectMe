cc8d7a6b2e0b681b7ba120cf511733f8b7ff4c67
mehrnaz98 <mehrnaz.eft98@gmail.com>
2025-12-11 16:20:16 +0330

    Create AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-[#59B2AC] py-8 mt-10 shadow-inner">
+    <div class="max-w-5xl mx-auto flex flex-col md:flex-row items-center justify-between px-6">
+      <ul class="flex gap-6 text-white mb-4 md:mb-0">
+        <li>Home</li>
+        <li>About</li>
+        <li>How It Works</li>
+        <li>Contact</li>
+      </ul>
+      <p class="text-white">
+        © 2025 DotConnectMe – Prototype<br />
+        Built as an open-source project by
+        <a class="text-blue-600 font-semibold" href="https://github.com/mehrnaz98"
+          >Mehrnaz Eftekhari</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
