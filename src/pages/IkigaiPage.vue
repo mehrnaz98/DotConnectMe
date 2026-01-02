db8c09d9da52f8246d90cd0f5644d37f60492eb4
mehrnaz98 <mehrnaz.eft98@gmail.com>
2026-01-02 17:08:19 +0330

    Create page for Ikigai quiz and wire suggested careers to CareerResults + CareerDetails

---
+++ src/pages/IkigaiPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <!-- IKIGAI SECTION -->
+  <section class="px-6 py-16 max-w-5xl mx-auto bg-gray-50 rounded-xl shadow">
+    <IkigaiSection v-model:selectedCareers="suggestedCareers" />
+  </section>
+
+  <!-- CAREER RESULTS -->
+  <section
+    v-if="suggestedCareers.length"
+    class="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-6 px-6 py-10 max-w-7xl mx-auto"
+  >
+    <div>
+      <CareerResults :jobs="suggestedCareers" @select="selectJob" />
+    </div>
+
+    <CareerDetails :job="selectedJob" />
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import CareerResults from '../components/CareerResults.vue'
+import CareerDetails from '../components/CareerDetails.vue'
+import IkigaiSection from '../components/IkigaiSection.vue'
+
+const suggestedCareers = ref([]) // will get data from IkigaiSection
+const selectedJob = ref(null) // job selected from CareerResults
+
+function selectJob(job) {
+  selectedJob.value = job
+}
+</script>
