4fa4eeddd98727c752b09344d9530b5ff05118e1
mehrnaz98 <mehrnaz.eft98@gmail.com>
2026-01-02 17:05:48 +0330

    Create dedicated page for career filter + results

---
+++ src/pages/CareerFilterPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <section class="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-6 px-6 py-10 max-w-7xl mx-auto">
+    <div>
+      <CareerFilter :all-skills="allSkills" :all-interests="allInterests" @search="searchCareers" />
+
+      <CareerResults :jobs="filteredJobs" @select="selectJob" />
+    </div>
+
+    <CareerDetails :job="selectedJob" />
+  </section>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import jobs from '../data/jobs.json'
+import CareerFilter from '../components/CareerFilter.vue'
+import CareerResults from '../components/CareerResults.vue'
+import CareerDetails from '../components/CareerDetails.vue'
+
+const selectedSkills = ref([])
+const selectedInterests = ref([])
+const filteredJobs = ref([])
+const selectedJob = ref(null)
+
+const allSkills = [...new Set(jobs.flatMap((j) => j.skills))]
+const allInterests = [...new Set(jobs.flatMap((j) => j.interests))]
+
+function searchCareers({ skills, interests }) {
+  selectedSkills.value = skills
+  selectedInterests.value = interests
+
+  filteredJobs.value = jobs.filter(
+    (job) =>
+      job.skills.some((s) => skills.includes(s)) &&
+      job.interests.some((i) => interests.includes(i)),
+  )
+
+  selectedJob.value = null
+}
+
+function selectJob(job) {
+  selectedJob.value = job
+}
+</script>
