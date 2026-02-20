cfaf071c2ecf88ca7ab7f7d5ec272b30817efb9f
mehrnaz98 <mehrnaz.eft98@gmail.com>
2026-02-20 19:06:19 +0330

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '../pages/HomePage.vue'
 import CareerFilterPage from '../pages/CareerFilterPage.vue'
 import IkigaiPage from '../pages/IkigaiPage.vue'
+import CommunityView from '../pages/CommunityView.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: HomePage },
   { path: '/career-filter', name: 'CareerFilter', component: CareerFilterPage },
   { path: '/ikigai', name: 'Ikigai', component: IkigaiPage },
+  {
+    path: '/community',
+    name: 'community',
+    component: CommunityView,
+  },
 ]
 
 const router = createRouter({
